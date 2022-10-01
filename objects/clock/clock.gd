22e8584cee89c8684b750290b37858b27f2138b6
Martin Jakubik <martin@jakubik.info>
2022-10-02 01:06:43 +0200

    simple end screen

--- objects/clock/clock.gd
+++ objects/clock/clock.gd
@@ -5,3 +5,6 @@ func _process(delta):
     # animate the hand based on the timer
     var percentage = 1.0 - $timer.time_left / $timer.wait_time
     $hand.rotation = -PI/2 + percentage * PI*2
+
+func _on_timer_timeout():
+    get_tree().change_scene_to_file("res://scenes/end.tscn")
