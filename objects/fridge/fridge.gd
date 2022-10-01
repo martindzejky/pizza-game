78f25021570f033b23183bd027e7e78e0c0058de
Martin Jakubik <martin@jakubik.info>
2022-10-01 13:17:56 +0200

    spawning dough in fridge

--- objects/fridge/fridge.gd
+++ objects/fridge/fridge.gd
@@ -3,15 +3,33 @@ extends Sprite2D
 @export var closedSprite: Texture2D
 @export var openSprite: Texture2D
 
+@export var dough: PackedScene
+
 var isOpen: bool = false
 
 
 func _onInputEvent(viewport: Node, event: InputEvent, shapeId: int) -> void:
     if event is InputEventMouseButton:
         if event.button_index == MOUSE_BUTTON_LEFT:
-            if event.is_pressed():
-                isOpen = !isOpen
+            if not event.pressed:
                 if isOpen:
-                    texture = openSprite
+                    close()
                 else:
-                    texture = closedSprite
+                    open()
+
+# open the fridge and spawn a new dough
+func open() -> void:
+    isOpen = true
+    texture = openSprite
+
+    var newDough = dough.instantiate()
+    newDough.transform = $spawnPoint.transform
+    add_child(newDough)
+
+# close the fridge and destroy the dough inside if there is any
+func close() -> void:
+    isOpen = false
+    texture = closedSprite
+
+    for dough in get_tree().get_nodes_in_group("dough-fridge"):
+        dough.queue_free()
