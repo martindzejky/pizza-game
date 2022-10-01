a9fbd0dada786c7dafa82b6d20120365ddd06f24
Martin Jakubik <martin@jakubik.info>
2022-10-01 22:13:24 +0200

    fix missing set_input_as_handled

--- objects/dough/dough.gd
+++ objects/dough/dough.gd
@@ -49,6 +49,8 @@ func _onInputEvent(viewport: Node, event: InputEvent, shapeId: int) -> void:
     if event is InputEventMouseButton:
         if event.button_index == MOUSE_BUTTON_LEFT:
             if not event.pressed:
+                viewport.set_input_as_handled()
+
                 if isPicked:
                     useOrDrop()
 
--- objects/fridge/fridge.gd
+++ objects/fridge/fridge.gd
@@ -16,6 +16,8 @@ func _onInputEvent(viewport: Node, event: InputEvent, shapeId: int) -> void:
     if event is InputEventMouseButton:
         if event.button_index == MOUSE_BUTTON_LEFT:
             if not event.pressed:
+                viewport.set_input_as_handled()
+
                 if isOpen:
                     close()
                 else:
