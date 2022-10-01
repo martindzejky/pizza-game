47cb4fe76a5d90b38ca5c284a65a4995ec8ea7c5
Martin Jakubik <martin@jakubik.info>
2022-10-02 00:11:25 +0200

    add table which drops objects from hand

---
+++ objects/table/table.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+
+func _onInputEvent(viewport: Node, event: InputEvent, shapeId: int) -> void:
+    if viewport.is_input_handled(): return
+
+    if event is InputEventMouseButton:
+        if event.button_index == MOUSE_BUTTON_LEFT:
+            if not event.pressed:
+                viewport.set_input_as_handled()
+                print("table")
+
+                if not Hand.isEmpty():
+                    Hand.drop()
