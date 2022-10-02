ec1732af8bd15e9e3b5c2779d9ff610124b6d516
Martin Jakubik <martin@jakubik.info>
2022-10-02 16:54:17 +0200

    reveal mouse when going to end scene

--- objects/clock/clock.gd
+++ objects/clock/clock.gd
@@ -7,4 +7,7 @@ func _process(delta):
     $hand.rotation = -PI/2 + percentage * PI*2
 
 func _on_timer_timeout():
+    # reveal mouse if hidden
+    Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
     get_tree().change_scene_to_file("res://scenes/end.tscn")
