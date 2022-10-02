5107cca9660ce6b15b7cfa6afd73ba8d90a5f77f
Martin Jakubik <martin@jakubik.info>
2022-10-02 17:49:03 +0200

    wiggle sprite

--- autoload/effects.gd
+++ autoload/effects.gd
@@ -8,7 +8,12 @@ func wiggle(node: Node) -> void:
     var tween = node.create_tween()
     if not tween: return
 
-    tween.tween_property(node, "scale", Vector2(0.95, 1.08), 0.1).from_current()
-    tween.tween_property(node, "scale", Vector2(1.08, 0.95), 0.1).from_current()
-    tween.tween_property(node, "scale", Vector2(0.98, 1.04), 0.06).from_current()
-    tween.tween_property(node, "scale", Vector2(1.04, 0.98), 0.06).from_current()
+    # if there's a sprite, wiggle it instead
+    if node.has_node("sprite"):
+        node = node.get_node("sprite")
+
+    tween.tween_property(node, "scale", Vector2(-0.04, 0.08), 0.1).from_current().as_relative()
+    tween.tween_property(node, "scale", Vector2(0.08, -0.04), 0.1).from_current().as_relative()
+    tween.tween_property(node, "scale", Vector2(-0.02, 0.04), 0.06).from_current().as_relative()
+    tween.tween_property(node, "scale", Vector2(0.04, -0.02), 0.06).from_current().as_relative()
+    tween.tween_property(node, "scale", Vector2(1, 1), 0.02).from_current()
