055ef089bd067cb61c6e464acb1564009d81ad58
Martin Jakubik <martin@jakubik.info>
2022-10-02 16:48:37 +0200

    implement the end scene gallery

--- ui/pizza-gallery.gd
+++ ui/pizza-gallery.gd
@@ -1,7 +1,16 @@
 extends GridContainer
 
+# Displays all pizzas made in the day in a nice grid gallery.
+
+@export var pictureObj: PackedScene
+
 
 func _ready():
     for pizza in Pizzas.get_children():
+        var picture = pictureObj.instantiate()
+
         Pizzas.remove_child(pizza)
-        add_child(pizza)
+        picture.get_node("container/center").add_child(pizza)
+
+        var stars = picture.get_node("rating/center/stars")
+        stars.region_rect.size.x = floor(pizza.score * 16)
