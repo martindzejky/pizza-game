74ac7086e85d27b6b3315d17c580f28e3b160b7b
Martin Jakubik <martin@jakubik.info>
2022-10-02 20:29:16 +0200

    UI sounds

--- ui/exit-btn.gd
+++ ui/exit-btn.gd
@@ -2,4 +2,7 @@ extends Control
 class_name ExitBtn
 
 func _onClicked():
-	get_tree().quit()
+    get_tree().quit()
+
+func _on_mouse_entered():
+    Effects.sound("hover")
--- ui/start-btn.gd
+++ ui/start-btn.gd
@@ -2,4 +2,8 @@ extends Control
 
 
 func _onClicked():
+    Effects.sound("click")
     get_tree().change_scene_to_file("res://scenes/level.tscn")
+
+func _on_mouse_entered():
+    Effects.sound("hover")
