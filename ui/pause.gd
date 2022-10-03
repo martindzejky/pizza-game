b612dc89ce20ff438d5bb4cd8551b2ea5d1bbabf
Martin Jakubik <martin@jakubik.info>
2022-10-03 11:48:30 +0200

    reveal mouse when opening the pause menu

--- ui/pause.gd
+++ ui/pause.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+var wasMouseHidden := false
+
 func _input(event: InputEvent):
 
     # only care about ESC (on release)
@@ -9,3 +11,12 @@ func _input(event: InputEvent):
 
     get_tree().paused = not get_tree().paused
     $root.visible = get_tree().paused
+
+    # reveal mouse if it was hidden by the Hand
+    if Input.get_mouse_mode() == Input.MOUSE_MODE_HIDDEN:
+        Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+        wasMouseHidden = true
+    else:
+        if wasMouseHidden:
+            Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+            wasMouseHidden = false
