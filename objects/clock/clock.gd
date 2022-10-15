7ec82612537408e38468768d461e4bb645548888
Martin Jakubik <martin@jakubik.info>
2022-10-15 19:47:17 +0200

    add large and small clock hand and animations
    Large hand = 10 seconds
    Small hand = 3 minutes, after which the level ends

--- objects/clock/clock.gd
+++ objects/clock/clock.gd
@@ -1,11 +1,6 @@
 extends Node2D
 
 
-func _process(delta):
-    # animate the hand based on the timer
-    var percentage = 1.0 - $timer.time_left / $timer.wait_time
-    $hand.rotation = -PI/2 + percentage * PI*2
-
 func _on_timer_timeout():
     # reveal mouse if hidden
     Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
