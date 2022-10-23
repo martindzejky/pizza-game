4ea9f1d384c0f00712222896b9598e2581f20576
Martin Jakubik <martin@jakubik.info>
2022-10-23 21:23:30 +0200

    super simple tutorial in scene transitions and pause menu
    closes #5

--- autoload/transitions.gd
+++ autoload/transitions.gd
@@ -5,15 +5,21 @@ extends Node2D
 var targetScene: String = ''
 
 
+func _ready():
+    $layer.hide()
+    $layer/cover.modulate.a = 0
+    $layer/cover/center/container/text.visible = false
+
+
 # Transition to a new scene
 func transitionTo(scene: String, text := ''):
     if $animation.is_playing(): return
 
     if text != '':
-        $layer/cover/center/text.text = text
-        $layer/cover/center/text.visible = true
+        $layer/cover/center/container/text.text = text
+        $layer/cover/center/container/text.visible = true
     else:
-        $layer/cover/center/text.visible = false
+        $layer/cover/center/container/text.visible = false
 
     targetScene = scene
     $animation.play("transition")
